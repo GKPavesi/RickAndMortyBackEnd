7cb67f060b0a7e96c0c8f6d48daf9d0e3eafc1e4
GKPavesi <guipavesi@hotmail.com>
2023-07-22 01:25:47 -0300

    search log updated, getting the correct ip to log

--- src/utils/searchs_log.py
+++ src/utils/searchs_log.py
@@ -3,10 +3,17 @@ from flask import request
 
 def log_search(search_result, search_result_cached = False):
 
+    # ip_address = request.access_route[0]
+
+    # for ip in request.access_route:
+    #     print(ip)
+    
+    # print('remote addr', request.remote_addr)
+    # print('forwarded for', request.headers.get('X-Forwarded-For'))
+
     search_results_log_entry = SearchLogs(
         request_headers=dict(request.headers),
-        # ip_address=request.headers.get('X-Forwarded-For'),
-        ip_address=request.remote_addr,
+        ip_address=request.access_route[0],
         user_agent=request.headers.get('User-Agent'),
         endpoint=request.url,
         success = search_result.get('data').get('success'),
